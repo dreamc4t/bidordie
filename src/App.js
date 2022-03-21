f94cbbcf162f88a38c000e7b1f1bd2e28a88530d
bravojack <bravojack.contact@gmail.com>
2022-03-21 13:43:51 +0100

    Router
    react-router-dom installed
    Routing inserted into App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route
+} from "react-router-dom"
+
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 
-
 function App() {
   return (
-    <div className="App">
-      <Header />
-      asdasdasd
-      <Footer />
-    </div>
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          {/*Pages go here as <Route>*/}
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
