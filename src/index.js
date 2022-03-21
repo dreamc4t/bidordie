7cb2b59b7127fb4ff8078246b6ce9645a968b40d
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2022-03-21 13:31:45 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import "./CSS/footer.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import './CSS/header.css';
+import './CSS/footer.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+
 
 ReactDOM.render(
   <React.StrictMode>
