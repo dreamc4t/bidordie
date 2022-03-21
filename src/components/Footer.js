c5f4debf59d0f5da5d1c3678ca18187875ff947f
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2022-03-21 13:12:03 +0100

    Footer
    Fixat själva grundutseendet till Footern

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,31 @@
+import { FaAppStoreIos } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaGooglePlay } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaQuestionCircle } from "react-icons/fa";
+
 const Footer = () => {
   return (
-    <div className="footer">Footer</div>
+    <footer>
+      <div className="footer">
+        <p>
+        <FaGooglePlay size={25} />
+        <FaAppStoreIos size={25} />
+        </p>
+        <FaQuestionCircle size={25} />
+        <h2>BIDORDIE</h2>
+        <p>
+        <FaFacebook size={25} />
+        <FaInstagram size={25} />
+        <FaTwitter size={25} />
+        </p>
+        <button type="button" onClick={(e) => {
+        e.preventDefault();
+        window.location.href='http://localhost:3000';
+        }}>About Us</button>
+      </div>
+    </footer>
   )
 }
 
