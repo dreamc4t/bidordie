9c43cff4472180bca1f6b3562341891c37bb8777
Twizzt <90067743+Twizzt@users.noreply.github.com>
2022-03-22 07:33:25 +0100

    re

--- src/App.js
+++ src/App.js
@@ -25,7 +25,7 @@ function App() {
             <Route 
               exact
               path="/"
-              element={<AddAuctionPage />}
+              element={<AuctionList />}
             />
             <Route 
               path="/login"
