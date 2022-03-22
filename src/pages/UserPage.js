1f53484ccf0f2dd7f57b9f65ac7bcc90b4f84461
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-03-22 11:31:27 +0100

    Update UserPage.js

--- src/pages/UserPage.js
+++ src/pages/UserPage.js
@@ -1,4 +1,7 @@
 const UserPage = () => {
+    const userReferences = [
+        {}
+    ]
     return (
       <div className="user-page">  
           <div className='user-top'>
@@ -6,9 +9,16 @@ const UserPage = () => {
               <h1>Mona Lisa</h1>
               <h3>Fullstack developer & React genius </h3>
               <h4>Venice, Italy</h4>
+              <h5>0707-1337</h5>
+          </div>
+          <div className="user-cv">
+              CV visas här.
           </div>
           <div className="user-bio">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacus diam. Cras a dignissim erat, pulvinar varius metus. Suspendisse rutrum odio nec elementum consectetur. Nam dictum odio quis iaculis interdum. Vivamus vel ligula rutrum, mollis orci in, viverra mauris. Ut risus nulla, luctus eget sodales congue, suscipit quis nibh. Donec lacus diam, eleifend ut tellus id, hendrerit malesuada sapien. Pellentesque cursus purus sit amet dui pharetra finibus.
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In nec lacus diam. Cras a dignissim erat, pulvinar varius metus. Suspendisse rutrum odio nec elementum consectetur. Nam dictum odio quis iaculis interdum. Vivamus vel ligula rutrum, mollis orci in, viverra mauris. Ut risus nulla, luctus eget sodales congue, suscipit quis nibh. Donec lacus diam, eleifend ut tellus id, hendrerit malesuada sapien. Pellentesque cursus purus sit amet dui pharetra finibus.
+          </div>
+          <div className="user-references">
+              Referenser visas här.
           </div>
           <div className="previous-commisions">
               Tidigare uppdrag visas här
