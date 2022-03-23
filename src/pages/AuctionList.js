0f07de0a864e505261083902f4f43d76478b2bcc
AfroraGoxhuli <93594561+AfroraGoxhuli@users.noreply.github.com>
2022-03-23 14:19:34 +0100

    Senaste ändringarna i auction.js

--- src/pages/AuctionList.js
+++ src/pages/AuctionList.js
@@ -9,7 +9,13 @@ const AuctionList = ({}) => {
         <div className="auction-list">
             <SortBar />
             <div className="auction-container">
-                <Auction />
+            <Auction 
+                firstName={"Erik"}
+                lastName={"Sund"}
+                place= {"Malmö"}
+                availability={"1th-30th may"}
+                competence={"Java, HTML/CSS, Python, SQL, JavaScript"}
+                briefInformation={"Hej! Jag är en glad, trevlig och driven programmerare som älskar utmaningar. Jag håller huvudet kallt under pressade situationer och kan komma med egna idéer."} />
             </div>
         </div>
     );
