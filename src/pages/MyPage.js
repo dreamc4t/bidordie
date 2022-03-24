15a7ca93c2be93b6e96171d570e872c63324051d
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-03-24 16:34:00 +0100

    Commit 8

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -5,7 +5,8 @@ import UserPageMockAuctions from "../components/UserPageMockAuctions";
 
 
 const MyPage = () => {
-    const { data: user, userLoading } = useFetch("http://localhost:6001/auctions/1");
+    const {data: user, userLoading} = useFetch("http://localhost:6001/users/1");
+
   return (
     <div className="user-page-wrapper">
         {userLoading && <div> LOADING USER PAGE....</div>}
@@ -13,7 +14,7 @@ const MyPage = () => {
           userImage={user.imageURL}
           userFirstName={user.firstName}
           userLastName={user.lastName}
-          userShortDescript={user.competence.join(", ")}
+          /* userShortDescript={user.competence.join(", ")} */
           userLocation={user.town}
           userPhoneNumber={user.phone}
           userCV={user.CV}
