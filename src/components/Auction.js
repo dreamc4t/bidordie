5c03732d61eaa43ef9d9e3ec7b28d7ab6136982c
AfroraGoxhuli <93594561+AfroraGoxhuli@users.noreply.github.com>
2022-03-24 11:40:57 +0100

    senast ej klar

--- src/components/Auction.js
+++ src/components/Auction.js
@@ -1,10 +1,8 @@
 import {GoLocation} from "react-icons/go"
 import {AiFillCalendar} from "react-icons/ai"
-import {StarRating} from "react-star-rating-element"
+/*import { Rating } from "react-simple-star-rating";*/
 
-const Auction = ({firstName,lastName,place,briefInformation,imageUrl,availability, competence}) => {
-
-
+const Auction = ({firstName,lastName,place,briefInformation,imageUrl,availability, competence, setRating}) => {
 
     return (
         <div className="auction">
@@ -17,16 +15,6 @@ const Auction = ({firstName,lastName,place,briefInformation,imageUrl,availabilit
                     <p><GoLocation/>{place}</p>
                     <p><AiFillCalendar/>{availability}</p>
                 </div>
-                <div className="Wrapper">
-                    <StarRating
-                        ratingValue={rating}
-                        changeRating={setRating}
-                        starEmptyColor="#999999"
-                        starSpacing={10}
-                        starDimension={30} />
-                </div>
-
-                
                 <div className="briefInfo">
                     <p className="boldText">About me</p>
                     <p>{briefInformation}</p>
@@ -47,5 +35,36 @@ place= {auction.place}
 availability={auction.availability}
 competence={auction.competence}
 briefInformation={auction.briefInformation}*/
+
+/*
+<div className="rating">
+     <Rating 
+        ratingValue={Rating}
+        changeRating={setRating}
+        starEmptyColor="#999999"
+        starSpacing={10}
+        starDimension={30} />
+</div>*/
+
+/*const Rating = () => {
+        const [rating, setRating] = useState(0);
+        return (
+          <div className="star-rating">
+            {[...Array(5)].map((star, index) => {
+              index += 1;
+              return (
+                <button
+                  type="button"
+                  key={index}
+                  className={index <= rating ? "on" : "off"}
+                  onClick={() => setRating(index)}
+                >
+                  <span className="star">&#9733;</span>
+                </button>
+              );
+            })}
+          </div>
+        );
+      };*/
 
 export default Auction 
