45c815928dd198668391197079922643adc40f71
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-03-25 11:05:34 +0100

    Commit 1 - Functionality

--- src/pages/MyPage.js
+++ src/pages/MyPage.js
@@ -1,17 +1,19 @@
 import useFetch from "../customHooks/useFetch"
 import UserPage from "./UserPage";
 import UserPageMockAuctions from "../components/UserPageMockAuctions";
+import { Link, Navigate } from "react-router-dom"
 
 
-
-const MyPage = () => {
-    const {data: user, userLoading} = useFetch("http://localhost:6001/users/1");
+const MyPage = ({loggedInUserID}) => {
+    const {data: user, userLoading} = useFetch("http://localhost:6001/users/" + loggedInUserID);
 
   return (
     <div className="user-page-wrapper">
+      {console.log("Hej hej hej", {loggedInUserID} )}
+      {loggedInUserID == null && < Navigate to="/" />}
         {userLoading && <div> LOADING USER PAGE....</div>}
         {user && <UserPage
-          userImage={user.imageURL}
+          userImage={user.imageUrl}
           userFirstName={user.firstName}
           userLastName={user.lastName}
           userShortDescript={user.competence.join(", ")}
