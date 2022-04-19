a743f2ea6f6e00fb8ac6f24341d1a39fc829178b
dreamc4t <eriksund1@gmail.com>
2022-04-19 09:59:29 +0200

    Adding bidordie-spring folder

---
+++ bidordie-spring/src/main/java/com/example/bidordiespring/BidordieSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.bidordiespring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BidordieSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BidordieSpringApplication.class, args);
+	}
+
+}
---
+++ bidordie-spring/src/test/java/com/example/bidordiespring/BidordieSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bidordiespring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BidordieSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
