85516f95c27af69b22484732c5962fafeccbffcd
Twizzt <90067743+Twizzt@users.noreply.github.com>
2022-04-19 21:11:11 +0200

    Update Auction.java

--- bidordie-spring/src/main/java/com/example/bidordiespring/models/Auction.java
+++ bidordie-spring/src/main/java/com/example/bidordiespring/models/Auction.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.validation.constraints.NotBlank;
-import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 
