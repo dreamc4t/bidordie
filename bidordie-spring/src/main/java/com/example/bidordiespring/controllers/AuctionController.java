abf4a30815f59ad8f96ca7850e1876c9a4b992e3
AfroraGoxhuli <93594561+AfroraGoxhuli@users.noreply.github.com>
2022-04-19 20:40:13 +0200

    test2

--- bidordie-spring/src/main/java/com/example/bidordiespring/controllers/AuctionController.java
+++ bidordie-spring/src/main/java/com/example/bidordiespring/controllers/AuctionController.java
@@ -2,6 +2,7 @@ package com.example.bidordiespring.controllers;
 
 
 import com.example.bidordiespring.models.Auction;
+import com.example.bidordiespring.models.User;
 import com.example.bidordiespring.payload.response.MessageResponse;
 import com.example.bidordiespring.repository.AuctionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 
 @CrossOrigin(origins = "*", maxAge = 3600)
@@ -19,7 +21,10 @@ public class AuctionController {
     @Autowired
     AuctionRepository auctionRepository;
 
-
+    //@GetMapping("/all")
+    //public List<Auction> getAllAuctions() {
+        //return auctionRepository.findAll();
+    //}
 
     @PostMapping("/create")
     public ResponseEntity<?> createAuction(@Valid @RequestBody Auction auctionRequest){
