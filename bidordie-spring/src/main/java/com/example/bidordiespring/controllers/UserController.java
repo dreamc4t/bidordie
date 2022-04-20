5400dab64aa747e35a3f09cdd8225411bc6b429a
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-04-20 13:41:49 +0200

    editUserById klart
    Varning: fält som inte får ny input i postman sätts till null. Kommer att åtgärda detta senare.

--- bidordie-spring/src/main/java/com/example/bidordiespring/controllers/UserController.java
+++ bidordie-spring/src/main/java/com/example/bidordiespring/controllers/UserController.java
@@ -5,9 +5,12 @@ import com.example.bidordiespring.models.User;
 import com.example.bidordiespring.payload.request.UserRequest;
 import com.example.bidordiespring.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 
@@ -28,6 +31,46 @@ public class UserController {
     public Optional<User> getUserById(@PathVariable String id) {
         return userRepository.findUserById(id);
     }
+
+    @PutMapping("/editUserById/{id}")
+    public ResponseEntity<?> editUser(@PathVariable("id") String id, @RequestBody UserRequest userRequest) {
+        Optional<User> userData = userRepository.findById(id);
+        if (userData.isPresent()) {
+            User user = userData.get();
+            user.setFirstName(userRequest.getFirstName());
+            user.setLastName(userRequest.getLastName());
+            user.setEmail(userRequest.getEmail());
+            user.setPassword(userRequest.getEmail());
+            user.setImageUrl(userRequest.getImageUrl());
+            user.setCvUrl(userRequest.getCvUrl());
+            user.setPhone(userRequest.getPhone());
+            user.setAddress(userRequest.getAddress());
+            user.setZipCode(userRequest.getZipCode());
+            user.setTown(userRequest.getTown());
+            user.setGithubLink(userRequest.getGithubLink());
+            user.setLinkedinLink(userRequest.getLinkedinLink());
+            user.setOtherLinks(userRequest.getOtherLinks());
+            user.setOtherInfo(userRequest.getOtherInfo());
+            user.setBiography(userRequest.getBiography());
+            user.setCompetence(userRequest.getCompetence());
+            // user.setAuctions(userRequest.getAuctionsId());
+            return new ResponseEntity<>(userRepository.save(user), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    /*@PutMapping("/editUserById/{id}")
+    public ResponseEntity<User> editUser(@PathVariable String id, @Valid @RequestBody User user) {
+        Optional<User> userToEdit = userRepository.findById(id));
+        if (!userToEdit.isPresent()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        if(userToEdit.get().getFirstName() != null) {
+            userToEdit.get().setFirstName(user.getFirstName());
+        }
+
+    }*/
 
     @DeleteMapping("deleteUserById/{id}")
     public User deleteUser(@PathVariable String id) {
