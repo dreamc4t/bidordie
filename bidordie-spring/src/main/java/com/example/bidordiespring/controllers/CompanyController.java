695b594b715d9107ae219c09df1a5241e213de46
henrikkihlberg <93766878+henrikkihlberg@users.noreply.github.com>
2022-04-20 14:32:19 +0200

    Update CompanyController.java

--- bidordie-spring/src/main/java/com/example/bidordiespring/controllers/CompanyController.java
+++ bidordie-spring/src/main/java/com/example/bidordiespring/controllers/CompanyController.java
@@ -1,9 +1,12 @@
 package com.example.bidordiespring.controllers;
 
 import com.example.bidordiespring.models.Company;
+import com.example.bidordiespring.models.User;
 import com.example.bidordiespring.payload.request.CompanyRequest;
+import com.example.bidordiespring.payload.request.UserRequest;
 import com.example.bidordiespring.repository.CompanyRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,6 +29,29 @@ public class CompanyController {
     @GetMapping("/getCompanyById/{id}")
     public Company getCompanyById(@PathVariable String id) {
         return companyRepository.findCompanyById(id);
+    }
+
+    @PutMapping("/editCompanyById/{id}")
+    public ResponseEntity<?> editCompany(@PathVariable("id") String id, @RequestBody CompanyRequest companyRequest) {
+        Optional<Company> companyData = companyRepository.findById(id);
+        if (companyData.isPresent()) {
+            Company company = companyData.get();
+            company.setCompanyName(companyRequest.getCompanyName());
+            company.setOrgNr(companyRequest.getOrgNr());
+            company.setEmail(companyRequest.getEmail());
+            company.setPassword(companyRequest.getPassword());
+            company.setImageUrl(companyRequest.getImageUrl());
+            company.setPhone(companyRequest.getPhone());
+            company.setAddress(companyRequest.getAddress());
+            company.setZipCode(companyRequest.getZipCode());
+            company.setTown(companyRequest.getTown());
+            company.setWebpage(companyRequest.getWebpage());
+            company.setOtherLinks(companyRequest.getOtherLinks());
+            company.setCompanyInfo(companyRequest.getCompanyInfo());
+            return new ResponseEntity<>(companyRepository.save(company), HttpStatus.OK);
+        } else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/deleteCompanyById/{id}")
