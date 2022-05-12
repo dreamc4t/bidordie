f20cb4391a15ba1cebbd43aa02767d92b20b0480
dreamc4t <eriksund1@gmail.com>
2022-05-12 17:02:23 +0200

    Mer constant fix

--- src/constants/urlConstants.js
+++ src/constants/urlConstants.js
@@ -1,13 +1,12 @@
 const URL_HOME = 'http://localhost:'
+//const URL_HOME = 'http://192.168.1.254:'
+// byts denna till ens lokala IP adress kan man köra den på hela nätverket!
+// Hade varit guld med en hämtad lokal ip som default, verkar mkt svårt dock 
+
+
 const API_URL_COMPANIES = URL_HOME + "8080/api/companies";
 const API_URL_FILES = URL_HOME + "8080/api/files";
 const API_URL_USERS = URL_HOME + "8080/api/users";
-
-
-//Vi vill ha local ip adress genererad här tack
-const IP_ADRESS = "http://192.168.1.254";
-
-
 
 
 export {URL_HOME, API_URL_COMPANIES, API_URL_FILES, API_URL_USERS}
