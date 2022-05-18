4004b30536da24cb02f9731388389f99a93fbe6e
Marcus921 <93766924+Marcus921@users.noreply.github.com>
2022-05-18 08:34:15 +0200

    Update

--- src/services/FaqMessageService.js
+++ src/services/FaqMessageService.js
@@ -40,19 +40,3 @@ class FaqMessageService {
 }
 
 export default new FaqMessageService();
-
-/* import axios from "axios";
-
-// axios.defaults.headers.post['Access-Control-Allow-Origin'] = '*';
-
-const URL_API = "http://localhost:5099/api/Destination/"
-
-const getAllDestinations = () => {
-    return axios.get(URL_API)
-}
-
-const DeliveryService ={
-    getAllDestinations
-}
-
-export default DeliveryService; */
